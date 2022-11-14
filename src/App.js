fe0bf01824cbd71c51b6286804b8f173aa876641
andreibodrov <ap.bodrov@gmail.com>
2022-11-14 23:33:34 +0300

    add schema and form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import validator from "@rjsf/validator-ajv8";
+import schema from './schema.json';
+import React from "react";
+import Form from "@rjsf/mui";
+import { Grid } from '@mui/material';
+import YAML from 'yaml';
+import Typography from '@mui/material/Typography';
 
-function App() {
+
+const App = () => {
+  const [formData, setFormData] = React.useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grid container spacing={2}>
+  <Grid item xs={6}>
+    <Typography variant={'h4'}>
+      <pre>{YAML.stringify(formData)}</pre>
+    </Typography>
+  </Grid>
+  <Grid item xs={6}>
+    <Form
+    schema={schema}
+    formData={formData}
+    onChange={e => setFormData(e.formData)}
+    validator={validator}
+  />
+  </Grid>
+</Grid>
+
+
   );
-}
+};
 
 export default App;
