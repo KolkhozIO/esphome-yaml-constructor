7f7e8f54612d9f72350c2af4d3cf10c5365ee48f
Kalibri1995 <gainatullinalba@gmail.com>
2023-06-14 20:56:45 +0300

    refactor url db

--- db/connect.py
+++ db/connect.py
@@ -1,9 +1,21 @@
+from os import environ
 from typing import Generator
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_POSTGRES_URL = 'postgresql+asyncpg://postgres:postgres@localhost:5432/postgres'
+DB_DRIVER = environ.get("DB_DRIVER")
+DB_COMMUNICATION_DRIVER = environ.get("DB_COMMUNICATION_DRIVER")
+DB_USER = environ.get("DB_USER")
+DB_PASSWORD = environ.get("DB_PASSWORD")
+DB_HOST = environ.get("DB_HOST")
+DB_PORT = environ.get("DB_PORT")
+DB_NAME = environ.get("DB_NAME")
+
+
+SQLALCHEMY_DATABASE_POSTGRES_URL = (
+    f"{DB_DRIVER}+{DB_COMMUNICATION_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_POSTGRES_URL, future=True, echo=True)
 
